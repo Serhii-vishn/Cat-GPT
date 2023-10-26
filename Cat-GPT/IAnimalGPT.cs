631ea486ce8af6095e9cc8e8a0b1a8967212a7ed
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-26 23:51:36 +0300

    Add interface IAnimalGPT

---
+++ Cat-GPT/IAnimalGPT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_GPT
+{
+    interface IAnimalGPT
+    {
+        string GenerateResponse();
+        void Speak(string message);
+
+        void LogData();
+
+        //string GetChatHistory(string chatName); just idea
+        //string SaveChatHistory(string lineChat);
+    }
+}
