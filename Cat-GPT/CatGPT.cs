cee4453c6644e2753fb65ae0e5f34543e4b93458
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-26 23:52:40 +0300

    Add CatGPT class

---
+++ Cat-GPT/CatGPT.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_GPT
+{
+    class CatGPT : IAnimalGPT
+    {
+        private readonly List<string> possibleResponses;
+        private readonly List<char> possibleSymbols;
+
+        CatGPT()
+        {
+            possibleResponses = new List<string>{
+                "meow",
+                "mew",
+                "meeeow",
+                "miw",
+                "meeow",
+                "meoow",
+                "meeeoww",
+                "meooow",
+                "meeeeeow",
+                "meeeeow",
+                "meooww",
+                "meeeeeeow",
+                "meeeeoow",
+                "meeeooow",
+                "meeeeeeoww",
+                "meeeeoooow",
+                "meoww",
+                "meoooww",
+                "meeoowww",
+                "meeeeooooww"
+            };
+
+            //possibleSymbols = new List<char>{
+            //    "meow",
+            //    "mew",
+            //    "meeeow",
+            //    "miw",
+            //    "meeow",
+            //    "meoow",
+            //    "meeeoww",
+            //    "meooow",
+            //    "meeeeeow",
+            //    "meeeeow",
+            //    "meooww",
+            //    "meeeeeeow",
+            //    "meeeeoow",
+            //    "meeeooow",
+            //    "meeeeeeoww",
+            //    "meeeeoooow",
+            //    "meoww",
+            //    "meoooww",
+            //    "meeoowww",
+            //    "meeeeooooww"
+            //};
+        }
+
+        public string GenerateResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Speak(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
