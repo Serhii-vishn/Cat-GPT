302f4b082dde354133d57f2f211c2a01470f3c50
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-27 12:36:01 +0300

    Add new interface IChatLogger for future logging data

--- Cat-GPT/IAnimalGPT.cs
+++ Cat-GPT/IAnimalGPT.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cat_GPT
+﻿namespace Cat_GPT
 {
     public interface IAnimalGPT
     {
---
+++ Cat-GPT/IChatLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Cat_GPT
+{
+    public interface IChatLogger
+    {
+        void LogData();
+        string GetChatHistory(string chatName);
+        string SaveChatHistory(string lineChat);
+    }
+}
