de37a4a19c59938aa7b569ffe31f128cec9b32cf
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-11-08 10:54:21 +0200

    Rename and rebuild IchatLogger to IchatHistory

---
+++ Cat-GPT/IChatHistory.cs
@@ -0,0 +1,9 @@
+﻿namespace Cat_GPT
+{
+    public interface IChatHistory
+    {
+        void SaveChatHistory(string fileName, List<string> chatHistory);
+        List<string> LoadChatHistory(string fileName);
+        List<string> GetChatList();
+    }
+}
--- Cat-GPT/IChatLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cat_GPT
-{
-    public interface IChatLogger
-    {
-        void LogData();
-        string GetChatHistory(string chatName);
-        string SaveChatHistory(string lineChat);
-    }
-}
