8f06cfab00b13194a935bb93eac5a91603186615
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-11-17 23:14:55 +0200

    refactor code of main

--- Cat-GPT/Program.cs
+++ Cat-GPT/Program.cs
@@ -10,49 +10,66 @@ internal class Program
             Console.WriteLine("\t\tHello! Welcome to Animal-GPT");
             Console.WriteLine("\t-------------------------------------------");
             Console.WriteLine("You can interact with the animals on the list below:" +
-                                                                    "\n\tCat - 1" +
-                                                                    "\n\tDog - 2" +
-                                                                    "\n\tExit - *");
-            Console.Write("\tEnter: "); var keyInput = Console.ReadKey().KeyChar;
+                                                                "\n\tCat - 1" +
+                                                                "\n\tDog - 2" +
+                                                                "\n\tExit - *");
+            Console.Write("\tEnter: ");
+            var keyInput = Console.ReadKey().KeyChar;
 
             Console.WriteLine("\n");
 
-            switch (keyInput)
-            {
-                case '1':
-                    {
-                        Console.WriteLine("To end the chat, send - #");
-                        bool key = true;
-                        while(key)
-                        {                        
-                            CatGPT catChat = new();
-                            Console.Write("\nSend a message: "); string userMess = Console.ReadLine();
-                            if (userMess == "#")
-                                key = false;
-                            else
-                                Console.WriteLine("Response: " + catChat.GenerateResponse(userMess));
-                        }
-                        break;
-                    }
-                case '2':
-                    {
-
-                        break;
-                    }
-                case '*':
-                    {
-                        Console.WriteLine("\t\tThank you for using, have a nice day ;)");
-                        Console.WriteLine("\t-------------------------------------------------------");
-                        Console.ReadLine();
-                        return;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Error! Invalid symbol. Try Again");
-                        break;
-                    }
-            }
-            Console.ReadLine();
+            HandleUserInput(keyInput);
         }
     }
+
+    private static void HandleUserInput(char keyInput)
+    {
+        switch (keyInput)
+        {
+            case '1':
+                HandleCatInteraction();
+                break;
+            case '2':
+                HandleDogInteraction();
+                break;
+            case '*':
+                ExitApplication();
+                break;
+            default:
+                Console.WriteLine("Error! Invalid symbol. Try Again");
+                break;
+        }
+
+        Console.ReadLine(); // Це може бути винесено за межі циклу while, якщо ви хочете чекати натискання клавіші тільки після введення користувача.
+    }
+
+    private static void HandleCatInteraction()
+    {
+        Console.WriteLine("To end the chat, send - #");
+        bool key = true;
+        while (key)
+        {
+            CatGPT catChat = new();
+            Console.Write("\nSend a message: ");
+            string userMess = Console.ReadLine();
+            if (userMess == "#")
+                key = false;
+            else
+                Console.WriteLine("Response: " + catChat.GenerateResponse(userMess));
+        }
+    }
+
+    private static void HandleDogInteraction()
+    {
+        
+    }
+
+    private static void ExitApplication()
+    {
+        Console.WriteLine("\t\tThank you for using, have a nice day ;)");
+        Console.WriteLine("\t-------------------------------------------------------");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+
 }
